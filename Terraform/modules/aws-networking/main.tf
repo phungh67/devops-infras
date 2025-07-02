8e3749c48c46f0a87c1c21f8fb59480cfb869dce
phungh67 <106119538+phungh67@users.noreply.github.com>
2025-07-02 20:35:47 +0700

    add: construct networking modules
    Module contains: VPC, subnets, route tables, internet gateway creation.

---
+++ Terraform/modules/aws-networking/main.tf
@@ -0,0 +1,70 @@
+locals {
+  Owner = "huyhoang.ph"
+  Type  = "IaC"
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = var.main_cidr
+  enable_dns_hostnames = true
+  tags = {
+    "Name"  = "${var.res_prefix}-main-vpc"
+    "Owner" = local.Owner
+    "Type"  = local.Type
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  vpc_id            = aws_vpc.main_vpc.id
+  count             = length(var.public_subnets_cidr)
+  cidr_block        = element(var.public_subnets_cidr, count.index)
+  availability_zone = element(var.az_list, count.index)
+
+  tags = {
+    "Name"  = "${var.res_prefix}-pub-subnet-00${count.index + 1}"
+    "Owner" = local.Owner
+    "Type"  = local.Type
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  vpc_id            = aws_vpc.main_vpc.id
+  count             = length(var.private_subnets_cidr)
+  cidr_block        = element(var.private_subnets_cidr, count.index)
+  availability_zone = element(var.az_list, count.index)
+
+  tags = {
+    "Name"  = "${var.res_prefix}-pri-subnet-00${count.index + 1}"
+    "Owner" = local.Owner
+    "Type"  = local.Type
+  }
+}
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    "Name"  = "${var.res_prefix}-main-igw"
+    "Owner" = local.Owner
+    "Type"  = local.Type
+  }
+}
+
+resource "aws_route_table" "pub_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    "Name"  = "${var.res_prefix}-pub-route-table"
+    "Owner" = local.Owner
+    "Type"  = local.Type
+  }
+}
+
+resource "aws_route" "internet_public_route" {
+  route_table_id         = aws_route_table.pub_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main_igw.id
+}
+
+resource "aws_route_table_association" "public_association" {
+  count          = length(var.public_subnets_cidr)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.pub_route_table.id
+}
---
+++ Terraform/modules/aws-networking/variables.tf
@@ -0,0 +1,35 @@
+variable "res_prefix" {
+  description = "The prefix in resource's name to indicate environment and region"
+  type        = string
+  default     = "due1"
+}
+
+variable "az_list" {
+  description = "The az that are associated with the region above"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "main_cidr" {
+  description = "Main CIDR, assigned to the management or the main VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "private_subnets_cidr" {
+  description = "CIDR for several private subnets, default is a set of 3"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.21.0/24", "10.0.31.0/24"]
+}
+
+variable "public_subnets_cidr" {
+  description = "CIDR for several public subnets, default is a set of 3"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.20.0/24", "10.0.30.0/24"]
+}
+
+variable "database_subnets_cidr" {
+  description = "CIDR for several database subnets, default is a set of 3"
+  type        = list(string)
+  default     = ["10.0.12.0/24", "10.0.22.0/24", "10.0.32.0/24"]
+}
