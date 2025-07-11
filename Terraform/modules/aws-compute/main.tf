2d97d148668e74f294e649dc683f2efea8ef8da7
phungh67 <106119538+phungh67@users.noreply.github.com>
2025-07-11 20:23:52 +0700

    add: Construct bastion host and NAT instance

--- Terraform/modules/aws-compute/main.tf
+++ Terraform/modules/aws-compute/main.tf
@@ -1,4 +1,38 @@
-resource "aws_instance" "bastion_host" {
-  ami = var.instance_default_ami[0]
+locals {
+  Owner = "huyhoang.ph"
+  Type  = "IaC"
+}
 
+resource "aws_instance" "bastion_host" {
+  ami             = var.instance_default_ami[0]
+  instance_type   = var.instance_type_list[0]
+  key_name        = var.common_key_name
+  subnet_id       = var.public_subnets_list[0]
+  security_groups = ["${var.bastion_sg}", "${var.internet_allow_sg}"]
+  metadata_options {
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 2
+  }
+  tags = {
+    "key"   = "${var.res_prefix}-bastion-host"
+    "Owner" = local.Owner
+    "Type"  = local.Type
+  }
+}
+
+resource "aws_instance" "nat_instance" {
+  ami             = var.nat_instance_base_image
+  instance_type   = var.instance_type_list[0]
+  key_name        = var.common_key_name
+  subnet_id       = var.public_subnet_list[1]
+  security_groups = ["${var.internet_allow_sg}", "${var.was_common_sg}"]
+  metadata_options {
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 2
+  }
+  tags = {
+    "Name"  = "${var.res_prefix}-nat-instance"
+    "Owner" = local.Owner
+    "Type"  = local.Type
+  }
 }
