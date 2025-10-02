73489b83f97cb9c07a94dfeb88ee8183f1b7ae07
huyhoang.ph <phungh67@gmail.com>
2025-10-02 23:19:26 +0200

    feat: parse with jq to find the account id

--- Bash/terraform-assume-role.sh
+++ Bash/terraform-assume-role.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
 # echo "Helper script to automatically retrieve and assume appropriate role"
+BASE_DIR="/tmp"
+EXTENSION="json"
 
+aws organizations list-accounts --query 'Accounts[*].[Name, Arn]' | jq -r '.[]' > "${BASE_DIR}/ouput"
 # hold the arguments
-ROLE_ARN=$(aws iam list-roles --query 'Roles[?contains(RoleName, `Organizations`)]' | jq -r '.[].Arn')
-ROLE_SESSION_NAME=$1
 
-# echo $ROLE_ARN
+sed 's/\] \[/\],\[/g; 1s/^/[ /; $s/$/ ]/' "${BASE_DIR}/ouput" > "${BASE_DIR}/ouput.${EXTENSION}"
 
-# check if arguments are passed
-if [ -z "$1" ]; then
-    echo "Error: missing ROLE_SESSION_NAME, try again with $0 <ROLE_SESSION_NAME>"
-    exit 1
-fi
+jq -c "${BASE_DIR}/ouput.${EXTENSION}" | while read -r line; do
+    # Extract alias and ARN
+    alias=$(echo "$line" | jq -r '.[0]')
+    arn=$(echo "$line" | jq -r '.[1]')
 
-echo "Will try to assume a role $ROLE_ARN with session name $1..."
-echo "==========================================================="
+    # Extract the numeric account ID from ARN
+    account_id=$(echo "$arn" | awk -F'/' '{print $NF}')
 
-aws sts assume-role \
-    --role-arn $ROLE_ARN \
-    --role-session-name $1
+    echo "Alias: $alias"
+    echo "ARN: $arn"
+    echo "Account ID: $account_id"
+    echo "---------------------------------"
+done
