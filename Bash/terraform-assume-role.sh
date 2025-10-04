1fc66c6eedce6f62a1e938aa608c15691c4a1b65
huyhoang.ph <phungh67@gmail.com>
2025-10-04 11:26:37 +0200

    feat: auto return dersire account ID within organization

--- Bash/terraform-assume-role.sh
+++ Bash/terraform-assume-role.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
+# Usage: ./script.sh <pattern>
+# Example: ./script.sh sandbox
 
-# echo "Helper script to automatically retrieve and assume appropriate role"
-BASE_DIR="/tmp"
-EXTENSION="json"
+PATTERN="$1"
 
-aws organizations list-accounts --query 'Accounts[*].[Name, Arn]' | jq -r '.[]' > "${BASE_DIR}/ouput"
-# hold the arguments
+if [ -z "$PATTERN" ]; then
+  echo "Usage: $0 <pattern>"
+  exit 1
+fi
 
-sed 's/\] \[/\],\[/g; 1s/^/[ /; $s/$/ ]/' "${BASE_DIR}/ouput" > "${BASE_DIR}/ouput.${EXTENSION}"
-
-jq -c "${BASE_DIR}/ouput.${EXTENSION}" | while read -r line; do
-    # Extract alias and ARN
-    alias=$(echo "$line" | jq -r '.[0]')
-    arn=$(echo "$line" | jq -r '.[1]')
-
-    # Extract the numeric account ID from ARN
-    account_id=$(echo "$arn" | awk -F'/' '{print $NF}')
-
-    echo "Alias: $alias"
-    echo "ARN: $arn"
-    echo "Account ID: $account_id"
-    echo "---------------------------------"
-done
+aws organizations list-accounts --query 'Accounts[*].[Name, Id]' --output json \
+  | jq -r --arg pat "$PATTERN" 'map(select(.[0] | test($pat; "i"))) | .[] | .[1]'                            
