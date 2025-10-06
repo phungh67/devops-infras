fa1a475e0d8fb63c147403e497123f57dff323c2
huyhoang.ph <phungh67@gmail.com>
2025-10-06 21:37:45 +0200

    feat: merge backend back to local

--- Terraform/environments/prod/backend.tf
+++ Terraform/environments/prod/backend.tf
@@ -1,7 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket = "deu1-state-bucket"
-    key    = "prod/terraform.tfstate"
-    region = "us-east-1"
+  backend "local" {
+    path = "./.local-state"
   }
 }
